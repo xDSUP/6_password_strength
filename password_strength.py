a9d19c423280ea8c340d08f5604c7473dd0e1a7e
xDSUP <ilya.elfimow@yandex.ru>
2016-12-31 11:12:50 +0300

    ver 1.1

--- password_strength.py
+++ password_strength.py
@@ -1,34 +1,80 @@
-import re
 from blacklist import blacklist
 
 personal_information = ["phone", 'address', 'email']
 
 
-def get_password_strength(password):
-    if password in blacklist:
-        return 1
-    if len(password) < 8:
+def check_password_len(password):
+    if len(password) > 12:
         return 2
-    if password.isdigit():
-        return 3
-    if password.isalpha():
-        return 4
-    if password.isupper():
-        return 5
-    if password.islower():
-        return 6
+    elif len(password) > 6:
+        return 1
+    else:
+        return 0
+
+
+def find_in_blacklist(password):
+    if password in blacklist:
+        return 0
+    else:
+        return 1
+
+
+def check_password_numbers(password):
+    if not password.isdigit():
+        numbers = 0
+        for char in password:
+            if char.isdigit():
+                numbers += 1
+        if numbers >= 4:
+            return 2
+        else:
+            return 1
+    else:
+        return 0
+
+
+def check_password_register(password):
+    if not password.isupper() and not password.islower():
+        return 2
+    else:
+        return 0
+
+
+def check_password_special_characters(password):
+    if not password.isalnum():
+        return 1
+    else:
+        return 0
+
+
+def check_password_letters(password):
+    if not password.isalpha():
+        return 1
+    else:
+        return 0
+
+
+def check_password_personal_info(password):
     if password in personal_information:
-        return 7
-    if password.isalnum():
-        return 8
-    if len(password) < 15:
-        return 9
-    return 10
+        return 0
+    else:
+        return 1
+
+
+def get_password_strength(password):
+    strength = 0
+    strength += check_password_len(password)
+    strength += find_in_blacklist(password)
+    strength += check_password_letters(password)
+    strength += check_password_numbers(password)
+    strength += check_password_personal_info(password)
+    strength += check_password_register(password)
+    strength += check_password_special_characters(password)
+    return strength
 
 if __name__ == '__main__':
-    while True:
-        user_password = input("Input you password :  ")
-        if user_password:
-            print("strength:", get_password_strength(user_password))
-        else:
-            break
+    user_password = input("Input you password :  ")
+    if user_password:
+        print("strength:", get_password_strength(user_password))
+    else:
+        print("you have not entered a password")
